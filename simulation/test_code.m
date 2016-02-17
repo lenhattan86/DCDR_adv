f2122c405609d8cb24b4f2a21465074c4dc6776d
lenhattan86 <lenhattan86@gmail.com>
2016-02-17 10:40:13 -0500

    syn

--- simulation/test_code.m
+++ simulation/test_code.m
@@ -1,20 +1,5 @@
-clear all; close all; clc;
-cvx_solver Gurobi;
+A = [5 6 1];
+B = [1 2 3 4; 1 4 2 1; 5 1 6 2];
 
-L = 3;
-T = 3;
-W = [0 0 1; 0 2 3; 1 3 4]
-lambda = [2 4 1];
-loadLevels = [0 0 0; 2 2 2; 4 4 4];
-
-cvx_begin
-    variable X(L,T) binary;
-    variable b(3);
-    minimize( sum(sum(W.*X)) );
-    subject to
-        sum(X)==ones(1,T);
-        X >= 0;
-        X <= 1;        
-        b == 0.5;
-        sum(loadLevels.*X) == lambda;
-cvx_end
+B_2 = reshape(B, 12, 1)
+A_2 = repmat(A, 3, 1)
